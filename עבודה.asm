86df1c7039d439548ead3d64cef06428506fb641
DvirWynne <133342953+DvirWynne@users.noreply.github.com>
2023-06-16 11:35:08 +0300

    עבודה.asm

--- עבודה.asm
+++ עבודה.asm
@@ -7,16 +7,16 @@ msg3 db 13,10, 'Enter the width you want your Square to be (1-9): $'
 msg4 db 13,10,'NUMBER CANT BE ZERO $'
 msg5 db 13,10,'ILLEGAL CHARACTER! $'
 msg6 db 13,10,'Enter the character for the square to be made of: $'
-crlf db 13,10,'$'
-len db 0
-mone dw 0
-counter dw 0        
-maxLines dw 0
-char db 0
+crlf db 13,10,'$'  ;Used to go to the next line
+len db 0           ;Uesd to store the length of each line
+mone dw 0          ;Use to store the width the user put
+counter dw 0       ;use to store the amount of time a line have been draw 
+maxLines dw 0      ;Use to store the length thhe user put
+char db 0          ;Used to store the char the square is made of
        
 .CODE
 
-     mov ax, @data
+     mov ax, @data  ; asking for length, width and a char
      mov ds, ax
 
      lea dx, msg2
@@ -29,9 +29,9 @@ char db 0
      cmp al, '0'     
      jl illegalchr  
      CMP al, '9'
-     Jg illegalchr
+     Jg illegalchr   ;check if the char is a num
      sub al, '0'
-     cmp al,0
+     cmp al,0        ;check if the num is zero
      je IsZero
      mov maxLines,ax
      
@@ -61,20 +61,20 @@ char db 0
      
      jmp nextLine  
 
-IsZero:
+IsZero:         ;if the number is zero IsZero print msg4 and exit the program
      lea dx, msg4
      mov ah, 09h
      int 21h
-     jmp exit
+     jmp exit                                                  
      
-illegalchr:
+illegalchr:     ;if the char isnt a number illegalchr print msg5 and exit the program
      lea dx, msg5
      mov ah, 09h
      int 21h
      jmp exit
 
      
-nextLine: 
+nextLine:  ;takes mone and put it in bx, call prtLine and incris the counter
      mov bx, mone
      push bx
      call prtLine
@@ -89,7 +89,7 @@ nextLine:
      
      jmp nextLine
          
-prtLine: 
+prtLine:      ;print an enter at the start of each line and put a space
      pop ax
      pop bx
      push ax
@@ -106,13 +106,13 @@ prtLine:
      mov dl, char
      mov ah, 02h
 
-prtChar: 
+prtChar:   ;print a series of the chars
      int 21h
      dec bl 
      jnz prtChar
      ret
      
-exit:
+exit:     ;exit the program
      lea dx, msg1
      mov ah, 09h
      int 21h
